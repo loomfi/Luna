9080ddbed25e7e9d0f3a3ec78be726a93da18488
Defy <61808223+De-y@users.noreply.github.com>
2024-07-19 14:18:30 -0700

    Ship 1.0.0

--- commands/core/about.js
+++ commands/core/about.js
@@ -4,6 +4,7 @@ module.exports = {
     data: new SlashCommandBuilder().setName('about').setDescription('Learn more about Luna'),
     async execute(ctx) {
         let embed = new EmbedBuilder().setTitle('🌙 Luna').setDescription('Written by Loom for utilization in the discord server, Luna is an open-source discord bot that can perform announcements and do moderation actions within settings. You can also make your own commands also.')
-        .setFooter("Created by Loom ❤️").setTimestamp()
+        .setFooter({text: "Created by Loom ❤️"}).setTimestamp()
+        await ctx.reply({embeds: [embed]})
     },
 }
